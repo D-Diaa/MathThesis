b7fd0c021efae76eee2bfb8143b3308e11dcdda6
D-Diaa <a.diaa@aucegypt.edu>
2020-09-21 00:52:46 +0200

    Added GGH cryptosystem with experiment script

---
+++ ggh.py
@@ -0,0 +1,64 @@
+from lattice import *
+
+
+def init_ggh(dim, d, good_h=0.8, bad_h=0.1):
+    V = gen_good_basis(dim, -d, d, good_h)
+    print(f"good h = {hadamard_ratio(V)}")
+    h = 1
+    while h > bad_h:
+        U = gen_rand_GLN_single(dim, d)
+        W = np.matmul(U, V)
+        h = hadamard_ratio(W)
+    print(f"bad h = {h}")
+    return U, V, W
+
+
+def encrypt(W, m, r):
+    return np.matmul(m, W) + r
+
+
+def decrypt(V, W, e):
+    v = babai_cvp(V, e)
+    _inv = lin.inv(W)
+    return np.rint(np.matmul(v, _inv))
+
+
+def example():
+    V = [
+        [-97, 19, 19],
+        [-36, 30, 86],
+        [-184, -64, 78]
+    ]
+    print(hadamard_ratio(V))
+    U = [
+        [4327, -15447, 23454],
+        [3297, -11770, 17871],
+        [5464, -19506, 29617]
+    ]
+    W = np.matmul(U, V)
+    print(hadamard_ratio(W))
+    m = [86, -35, -32]
+    r = [-4, -3, 2]
+    e = encrypt(W, m, r)
+    print(e)
+    good_m = decrypt(V, W, e)
+    print(good_m)
+    bad_m = decrypt(W, W, e)
+    print(bad_m)
+
+
+def ggh_experiment(d=300, delta=15, dim=10, good_h=0.99, bad_h=0.1):
+    U, V, W = init_ggh(dim, d, good_h, bad_h)
+    m = np.random.randint(-d, d, (1, dim))[0]
+    r = np.random.randint(-delta, delta, (1, dim))[0]
+    print(f"original m: {m}")
+    enc = encrypt(W, m, r)
+    good_m = np.array(decrypt(V, W, enc), dtype='int64')
+    print(f"dec_good m: {good_m}")
+    bad_m = np.array(decrypt(W, W, enc), dtype='int64')
+    print(f"dec_bad m: {bad_m}")
+
+
+if __name__ == '__main__':
+    # example()
+    ggh_experiment(dim=10, d=300, delta=5)
--- lattice.py
+++ lattice.py
@@ -1,6 +1,8 @@
 import numpy as np
-import numpy.linalg as lin
+import scipy.linalg as lin
 from utils import first_divisor
+from vector_spaces import gram_schmidt
+from math import pi, e, sqrt
 from random import randint
 
 EPS = 1e-9
@@ -10,9 +12,14 @@ def eps_eq(u, v):
     return u - EPS <= v <= u + EPS
 
 
+def is_integer_entries(mat):
+    ints = np.rint(mat)
+    return np.allclose(ints, mat)
+
+
 def in_GL(mat):
     det = lin.det(mat)
-    return eps_eq(det, 1) or eps_eq(det, -1)
+    return (eps_eq(det, 1) or eps_eq(det, -1)) and is_integer_entries(mat)
 
 
 def GL_inverse(mat):
@@ -22,11 +29,62 @@ def GL_inverse(mat):
         print("Not in GL")
 
 
-def gen_rand_GL2(samples, max=100):
+def gaussian_heuristic(dim, mat=None, det=None, nthroot_det=None):
+    sigma = sqrt(dim / (2 * pi * e))
+    if nthroot_det is None:
+        if det is None:
+            if mat is None:
+                return -1
+            else:
+                det = lin.det(mat)
+        nthroot_det = det ** (1 / dim)
+    return sigma * nthroot_det
+
+
+def gen_good_basis(dim, min_val=-100, max_val=100, min_h=0.8):
+    h = 0
+    mat = None
+    while h < min_h:
+        mat = np.array(np.random.randint(min_val, max_val, (dim, dim)), dtype='float64')
+        h = hadamard_ratio(mat)
+        if h < min_h:
+            mat = gram_schmidt(mat)
+            mat = np.rint(mat)
+            h = hadamard_ratio(mat)
+        if h != h:
+            h = 0
+    # print(f"found basis with h = {h}")
+    return mat
+
+
+def randomize_rows(mat):
+    dim = len(mat)
+    for i in range(dim):
+        swapInd = randint(i, dim - 1)
+        mat[[i, swapInd]] = mat[[swapInd, i]]
+    return mat
+
+
+def gen_rand_GLN_single(dim, max_val=100):
+    max = int(sqrt(2 * max_val / dim))
+    L = np.identity(dim)
+    U = np.identity(dim)
+    for i in range(0, dim):
+        for j in range(0, i):
+            L[i][j] = randint(1, max)
+            U[j][i] = randint(1, max)
+    ret = np.matmul(L, U)
+    ret = randomize_rows(ret)
+    ret = np.transpose(ret)
+    ret = randomize_rows(ret)
+    return ret
+
+
+def gen_rand_GL2(samples, max_val=100):
     res = []
     while len(res) <= samples:
-        b = randint(1, max)
-        c = randint(1, max)
+        b = randint(1, max_val)
+        c = randint(1, max_val)
         div = first_divisor(b * c + 1)
         if div != b * c + 1:
             a = div
@@ -56,6 +114,28 @@ def check_commutative(mat1, mat2):
 
 def change_of_basis(A, B):
     return np.matmul(B, lin.inv(A))
+
+
+def babai_cvp(bases, w):
+    bases = np.transpose(bases)
+    _inv = lin.inv(bases)
+    t = np.matmul(_inv, w)
+    a = np.rint(t)
+    return np.matmul(bases, a)
+
+
+def hadamard_ratio(bases):
+    dim = len(bases)
+    det = abs(lin.det(bases))
+    denom = 1
+    for base in bases:
+        denom *= lin.norm(base)
+    frac = det / denom
+    return frac ** (1 / dim)
+
+
+def babai_svp(bases):
+    pass
 
 
 def Q7():
@@ -130,8 +210,49 @@ def Q13():
         print("Not a basis")
 
 
+def Q16():
+    print(gaussian_heuristic(251, nthroot_det=2 ** (2251.58 / 251)))
+
+
+def Q17():
+    basis = [
+        [213, -437],  # v1
+        [312, 105]  # v2
+    ]
+    w = [43127, 11349]
+
+    v = babai_cvp(basis, w)
+    h = hadamard_ratio(basis)
+    d = lin.norm(v - w)
+    print(v)
+    print(d)
+    print(h)
+    ans = "Very good basis" if h >= 0.9 else "basis not very orthogonal"
+    print(ans)
+
+    new_basis = [
+        [2937, -1555],
+        [11223, -5888]
+    ]
+    M = change_of_basis(basis, new_basis)
+    print(M)
+    ans = "new basis is also a basis for L" if in_GL(M) else "not a new basis"
+    print(ans)
+
+    v = babai_cvp(new_basis, w)
+    h = hadamard_ratio(new_basis)
+    d = lin.norm(v - w)
+    print(v)
+    print(d)
+    print(h)
+    ans = "Very good basis" if h >= 0.9 else "basis not very orthogonal"
+    print(ans)
+
+
 if __name__ == '__main__':
-    Q7()
-    Q11()
-    Q12()
-    Q13()
+    # Q7()
+    # Q11()
+    # Q12()
+    # Q13()
+    # Q16()
+    Q17()
